5db0f9cea70886fcdbe42954fd1df93abbda1a0a
Victor Maus <maus@iiasa.ac.at>
2018-05-31 10:28:50 +0200

    Add beans as first and second crop.

--- main.R
+++ main.R
@@ -24,6 +24,10 @@ mt_mu %>% dplyr::select(NM_MUNICIP) %>%
 mt_crop_calendars <- readODS::ods_sheets("./input/planting_dates_mt_brazil.ods")
 mt_crop_calendars
 
+# Read municipality concordance table
+mu_concordance <- readODS::read_ods("./input/planting_dates_mt_brazil.ods", sheet = "concordance_ibge", col_names = TRUE) %>% 
+  tibble::as_tibble()
+
 # Create soybean spatiotemporal caledar 
 mt_soybean_caledar <- readODS::read_ods("./input/planting_dates_mt_brazil.ods", sheet = "soy", col_names = TRUE, skip = 1, range = "A1:D141") %>% 
   tibble::as_tibble() %>% 
@@ -42,7 +46,7 @@ mt_soybean_caledar <- readODS::read_ods("./input/planting_dates_mt_brazil.ods", 
 mt_soybean_caledar %>% 
   plot()
 
-# Create maize spatiotemporal caledar 
+# Create maize first crop spatiotemporal caledar 
 mt_corn_1_caledar <- readODS::read_ods("./input/planting_dates_mt_brazil.ods", sheet = "corn_1st_crop", col_names = TRUE, skip = 1, range = "A1:D141") %>% 
   tibble::as_tibble() %>% 
   dplyr::filter(Cod_Munic != "Total Result") %>% 
@@ -67,6 +71,8 @@ mt_cotton_caledar <- readODS::read_ods("./input/planting_dates_mt_brazil.ods", s
   dplyr::filter(Cod_Munic != "Total Result") %>% 
   dplyr::transmute(NM_MUNICIP = stringr::str_to_upper(Nome_Municipio), start = `Min - start_date`, end = `Max - end_date`) %>% 
   dplyr::mutate(label = "Soybean", start = lubridate::mdy(start), end = lubridate::mdy(end)) %>% 
+  dplyr::right_join(mu_concordance, by = c("NM_MUNICIP" = "name_1")) %>% 
+  dplyr::mutate(NM_MUNICIP = stringr::str_to_upper(ibge)) %>%
   dplyr::right_join(mt_mu, by = c("NM_MUNICIP" = "NM_MUNICIP")) %>% 
   sf::st_as_sf() %>% 
   dplyr::select(label, start, end) %>% 
@@ -79,5 +85,43 @@ mt_cotton_caledar <- readODS::read_ods("./input/planting_dates_mt_brazil.ods", s
 mt_cotton_caledar %>% 
   plot()
 
+# Create beans first crop spatiotemporal caledar 
+mt_beans_1_caledar <- readODS::read_ods("./input/planting_dates_mt_brazil.ods", sheet = "beans_1st_crop", col_names = TRUE, skip = 1, range = "A1:D141") %>% 
+  tibble::as_tibble() %>% 
+  dplyr::filter(Cod_Munic != "Total Result") %>% 
+  dplyr::transmute(NM_MUNICIP = stringr::str_to_upper(Nome_Municipio), start = `Min - start_date`, end = `Max - end_date`) %>% 
+  dplyr::mutate(label = "Soybean", start = lubridate::mdy(start), end = lubridate::mdy(end)) %>% 
+  dplyr::right_join(mu_concordance, by = c("NM_MUNICIP" = "name_1")) %>% 
+  dplyr::mutate(NM_MUNICIP = stringr::str_to_upper(ibge)) %>%
+  dplyr::right_join(mt_mu, by = c("NM_MUNICIP" = "NM_MUNICIP")) %>% 
+  sf::st_as_sf() %>% 
+  dplyr::select(label, start, end) %>% 
+  st_set_precision(1000) %>% 
+  lwgeom::st_make_valid() %>% 
+  dplyr::group_by(start, end) %>% 
+  dplyr::summarise() %>% 
+  dplyr::ungroup()
 
+mt_beans_1_caledar %>% 
+  plot()
+
+# Create beans second crop spatiotemporal caledar 
+mt_beans_2_caledar <- readODS::read_ods("./input/planting_dates_mt_brazil.ods", sheet = "beans_2nd_crop", col_names = TRUE, skip = 1, range = "A1:D141") %>% 
+  tibble::as_tibble() %>% 
+  dplyr::filter(Cod_Munic != "Total Result") %>% 
+  dplyr::transmute(NM_MUNICIP = stringr::str_to_upper(Nome_Municipio), start = `Min - start_date`, end = `Max - end_date`) %>% 
+  dplyr::mutate(label = "Soybean", start = lubridate::mdy(start), end = lubridate::mdy(end)) %>% 
+  dplyr::right_join(mu_concordance, by = c("NM_MUNICIP" = "name_1")) %>% 
+  dplyr::mutate(NM_MUNICIP = stringr::str_to_upper(ibge)) %>%
+  dplyr::right_join(mt_mu, by = c("NM_MUNICIP" = "NM_MUNICIP")) %>% 
+  sf::st_as_sf() %>% 
+  dplyr::select(label, start, end) %>% 
+  st_set_precision(1000) %>% 
+  lwgeom::st_make_valid() %>% 
+  dplyr::group_by(start, end) %>% 
+  dplyr::summarise() %>% 
+  dplyr::ungroup()
+
+mt_beans_2_caledar %>% 
+  plot()
 
